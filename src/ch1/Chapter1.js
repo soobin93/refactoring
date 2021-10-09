6827edefc17fdbb3fad0c772a5c1bad3a69f8421
Soobin <ssbin93@gmail.com>
2021-10-09 23:42:48 +1000

    Replace temporary variable volumeCredits to function

--- src/ch1/Chapter1.js
+++ src/ch1/Chapter1.js
@@ -11,23 +11,27 @@ const Chapter1 = () => {
 
   const statement = (invoice, plays) => {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-    for (let perf of invoice.performances) {
-
-      volumeCredits += volumeCreditsFor(perf);
-
+    for (const perf of invoice.performances) {
       // Print statement
       result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
       totalAmount += amountFor(perf);
     }
 
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`
     return result;
 
     // Nested functions below
+    function totalVolumeCredits() {
+      let result = 0;
+      for (let perf of invoice.performances) {
+        result += volumeCreditsFor(perf);
+      }
+      return result;
+    }
+
     function usd(aNumber) {
       return new Intl.NumberFormat('en-US', {
         style: 'currency',
