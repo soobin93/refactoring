458b026180e910ca6b209934b0e7783a884b51c8
Soobin <ssbin93@gmail.com>
2021-10-09 23:45:16 +1000

    Replace temporary variable totalAmount to function

--- src/ch1/Chapter1.js
+++ src/ch1/Chapter1.js
@@ -10,20 +10,26 @@ const Chapter1 = () => {
   }, []);
 
   const statement = (invoice, plays) => {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (const perf of invoice.performances) {
       // Print statement
       result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
-      totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`
     return result;
 
     // Nested functions below
+    function totalAmount() {
+      let result = 0;
+      for (const perf of invoice.performances) {
+        result += amountFor(perf);
+      }
+      return result;
+    }
+
     function totalVolumeCredits() {
       let result = 0;
       for (let perf of invoice.performances) {
