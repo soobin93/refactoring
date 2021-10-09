3d244af5f1f58e98f5113b84172c08d26c3726ba
Soobin <ssbin93@gmail.com>
2021-10-09 23:36:56 +1000

    Rename function format to usd

--- src/ch1/Chapter1.js
+++ src/ch1/Chapter1.js
@@ -19,21 +19,21 @@ const Chapter1 = () => {
       volumeCredits += volumeCreditsFor(perf);
 
       // Print statement
-      result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${perf.audience}석\n`;
+      result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
       totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`
     return result;
 
     // Nested functions below
-    function format(aNumber) {
+    function usd(aNumber) {
       return new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
         minimumFractionDigits: 2
-      }).format(aNumber);
+      }).format(aNumber / 100);
     }
 
     function volumeCreditsFor(aPerformance) {
