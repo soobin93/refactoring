dda0c6ff2604938da24b84e6521e83ccb91f2b51
Soobin <ssbin93@gmail.com>
2021-10-11 00:53:04 +1000

    Move playFor and amountFor functions from renderPlainText to statement

--- src/ch1/Chapter1.js
+++ src/ch1/Chapter1.js
@@ -15,16 +15,53 @@ const Chapter1 = () => {
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
 
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = {...aPerformance};
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+
+      default:
+        throw new Error(`Unknown Genre: ${aPerformance.play.type}`);
+    }
+
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (const perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} ${perf.audience}석\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -35,7 +72,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (const perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -59,40 +96,10 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if (playFor(aPerformance).type === 'comedy')
+    if (aPerformance.play.type === 'comedy')
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`Unknown Genre: ${playFor(aPerformance).type}`);
-    }
-
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
   }
 }
 
