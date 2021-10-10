05f20b98ff160e455bf2494f84511d134913dfd4
Soobin <ssbin93@gmail.com>
2021-10-11 01:42:22 +1000

    Implement htmlStatement

--- src/ch1/Chapter1.js
+++ src/ch1/Chapter1.js
@@ -1,16 +1,22 @@
 import React, {useEffect} from "react";
+
 import invoices from "./invoices.json";
 import plays from "./plays.json";
-import {statement} from "./statement";
+import {statement, htmlStatement} from "./statement";
 
 const Chapter1 = () => {
   useEffect(() => {
     invoices.forEach((invoice) => {
       console.log(statement(invoice, plays));
-    })
+    });
   }, []);
 
-  return (<></>);
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: invoices.map((invoice) => htmlStatement(invoice, plays)) }} />
+    </div>
+
+  );
 };
 
 
